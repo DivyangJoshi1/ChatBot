409326d3b0eef0d60886c0563eb46d2e98f8234b
DivyangJoshi1 <99973206+DivyangJoshi1@users.noreply.github.com>
2024-10-14 03:03:31 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Update package list and install Python build dependencies
-apt-get update && apt-get install -y python3-dev gcc build-essential
-
-# Install any other system-level dependencies your project might need
